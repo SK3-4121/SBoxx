3b07e983e7d14effd6892bb6c959b5bde235d795
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2021-08-25 00:01:12 -0700

    WARNING THIS RUNS AS ROOT

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+clear
+echo "#############################################"
+echo " ######### SBoxx Made by SK3#0001 ##########"
+echo "  #########################################\n\n"
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git wget python3.8 python apache2 php7.4 -y
+echo "Done updateing and upgrading and installing all of the main pakages"
+
+wget https://raw.githubusercontent.com/SK3-4121/SBoxx/main/Installer/launcher.py
+
+echo "|========= Completed install of launcher.py =========|"
+echo "\nNow runing launcher.py"
+
+sudo python3 launcher.py
