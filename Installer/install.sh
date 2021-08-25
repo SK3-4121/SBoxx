406f6a04ae07a46052c7d29340069544bd048d87
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2021-08-25 00:05:24 -0700

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 clear
 echo "#############################################"
 echo " ######### SBoxx Made by SK3#0001 ##########"
@@ -7,12 +6,9 @@ echo "  #########################################\n\n"
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install git wget python3.8 python apache2 php7.4 -y
+sudo apt install git wget python3.8 python apache2 php7.4 curl -y
 echo "Done updateing and upgrading and installing all of the main pakages"
 
-wget https://raw.githubusercontent.com/SK3-4121/SBoxx/main/Installer/launcher.py
+echo "|========= Completed install now closing and uninstalling this file =========|"
 
-echo "|========= Completed install of launcher.py =========|"
-echo "\nNow runing launcher.py"
-
-sudo python3 launcher.py
+sudo rm -f -r install.sh
