f6a36f975f6caef19e356dcaaa0d21aaadfe932e
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2021-08-25 00:07:03 -0700

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -8,6 +8,8 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git wget python3.8 python apache2 php7.4 curl -y
 echo "Done updateing and upgrading and installing all of the main pakages"
+git clone https://github.com/SK3-4121/SBoxx.git
+sudo chmod 777 SBoxx
 
 echo "|========= Completed install now closing and uninstalling this file =========|"
 
